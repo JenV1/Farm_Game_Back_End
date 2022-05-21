41b57ab356b20a1115a1e59019c1d5615f8189e7
Lambert-Simnel <Josephsut@gmail.com>
2022-05-21 16:42:34 +0100

    Models initial properties added

--- src/main/java/com/example/farm_game/models/Crop.java
+++ src/main/java/com/example/farm_game/models/Crop.java
@@ -1,4 +1,100 @@
 package com.example.farm_game.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Crops")
 public class Crop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private int price;
+
+    @Column
+    private int stock;
+
+    @Column
+    private int growtime;
+
+    @JsonIgnoreProperties({"crop"})
+    @OneToMany(mappedBy = "crop")
+    private List<Field> fields;
+
+    public Crop(Long id, String name, int price, int stock, int growtime, List<Field> fields) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.growtime = growtime;
+        this.fields = fields;
+    }
+
+    public Crop() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getGrowtime() {
+        return growtime;
+    }
+
+    public void setGrowtime(int growtime) {
+        this.growtime = growtime;
+    }
+
+    public List<Field> getFields() {
+        return fields;
+    }
+
+    public void setFields(List<Field> fields) {
+        this.fields = fields;
+    }
+
+    public void addField(Field field) {
+        this.fields.add(field);
+    }
+
+    public void deleteField(int fieldId) {
+        this.fields.remove(fieldId);
+    }
 }
--- src/main/java/com/example/farm_game/models/Field.java
+++ src/main/java/com/example/farm_game/models/Field.java
@@ -1,4 +1,94 @@
 package com.example.farm_game.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Fields")
 public class Field {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private int timeLeft;
+
+    @Column
+    @ManyToOne
+    @JoinColumn(name = "field_id", nullable = false)
+    private FieldType fieldType;
+
+    @Column
+    @ManyToOne
+    @JoinColumn(name = "crop_id", nullable = false)
+    private Crop crop;
+
+    @Column
+    @ManyToOne
+    @JoinColumn(name = "farm_id", nullable = false)
+    private Farm farm;
+
+    public Field(Long id, String name, int timeLeft, FieldType fieldType, Crop crop, Farm farm) {
+        this.id = id;
+        this.name = name;
+        this.timeLeft = timeLeft;
+        this.fieldType = fieldType;
+        this.crop = crop;
+        this.farm = farm;
+    }
+
+    public Field() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTimeLeft() {
+        return timeLeft;
+    }
+
+    public void setTimeLeft(int timeLeft) {
+        this.timeLeft = timeLeft;
+    }
+
+    public FieldType getFieldType() {
+        return fieldType;
+    }
+
+    public void setFieldType(FieldType fieldType) {
+        this.fieldType = fieldType;
+    }
+
+    public Crop getCrop() {
+        return crop;
+    }
+
+    public void setCrop(Crop crop) {
+        this.crop = crop;
+    }
+
+    public Farm getFarm() {
+        return farm;
+    }
+
+    public void setFarm(Farm farm) {
+        this.farm = farm;
+    }
 }
