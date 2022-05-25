61482c63d360e36f9b355f1373f6562f621632f6
Jenna Vlahos <Jenna_v@hotmail.co.uk>
2022-05-25 14:19:13 +0100

    sql query in fieldRepository fixed

--- src/main/java/com/example/farm_game/repositories/FieldRepository.java
+++ src/main/java/com/example/farm_game/repositories/FieldRepository.java
@@ -12,31 +12,39 @@ import javax.transaction.Transactional;
 @Repository
 public interface FieldRepository extends JpaRepository<Field,Long> {
 
-    String tablesToFindTotal = "WITH NEW_TABLE AS ( " +
-            "SELECT FIELDS.ID AS ID, FIELDS.FARM_ID AS farm, CROPS.SELLPRICE as price, " +
-            "FIELD_TYPES.SIZE AS size, CROP_SOIL_TYPES.SOIL_TYPES as soil_types, CROP_SOIL_EFFECTS.SOIL_EFFECTS as effects," +
-            "FROM FIELDS " +
-            "INNER JOIN FIELD_TYPES " +
-            "ON FIELDS.FIELD_ID = FIELD_TYPES.ID " +
-            "INNER JOIN CROPS " +
-            "ON FIELDS.CROP_ID = CROPS.ID " +
-            "LEFT JOIN CROP_SOIL_EFFECTS  " +
-            "ON CROPS.ID = CROP_SOIL_EFFECTS.CROP_ID " +
-            "LEFT JOIN CROP_SOIL_TYPES " +
-            "ON FIELDS.CROP_ID = CROP_SOIL_TYPES.SOIL_TYPES " +
-            "WHERE (SOIL_TYPES IS NULL AND SOIL_EFFECTS IS NULL) OR NOT  (SOIL_TYPES IS NULL AND SOIL_EFFECTS = 3) " +
-            "AND FIELDS.TIMELEFT = 0 AND FIELDS.CROP_ID IS NOT NULL), " +
-            "NO_DUPLICATE AS (SELECT " +
-            "    ID, farm, price, size, soil_types, MAX(effects) AS max " +
-            "FROM " +
-            "    NEW_TABLE " +
-            "GROUP BY " +
-            "    ID " +
-            "), " +
-            "DOUBLE_YIELDS AS (SELECT * FROM NO_DUPLICATE WHERE MAX = 3), " +
-            "HALF_YIELDS AS (SELECT * FROM NO_DUPLICATE WHERE MAX = 2 AND SOIL_TYPES IS NULL), " +
-            "STANDARD_YIELD AS (SELECT * FROM NO_DUPLICATE WHERE (SOIL_TYPES IS NULL AND MAX IS NULL) " +
-            " OR (MAX IN (0,1,2) OR MAX IS NULL) AND NOT (MAX = 2 AND SOIL_TYPES IS NULL) ) ";
+    String tablesToFindTotal = "WITH NEW_TABLE AS (\n" +
+            "SELECT \n" +
+            "FIELDS.ID AS ID, FIELDS.FARM_ID AS farm, CROPS.SELLPRICE " +
+            "as price, FIELD_TYPES.SIZE AS size, CROP_SOIL_TYPES.SOIL_TYPES as soil_types, " +
+            "CROP_SOIL_EFFECTS.SOIL_EFFECTS as effects,\n" +
+            "FROM FIELDS \n" +
+            "INNER JOIN FIELD_TYPES\n" +
+            "ON FIELDS.FIELD_ID = FIELD_TYPES.ID\n" +
+            "INNER JOIN CROPS\n" +
+            "ON FIELDS.CROP_ID = CROPS.ID\n" +
+            "LEFT JOIN CROP_SOIL_EFFECTS \n" +
+            "ON CROPS.ID = CROP_SOIL_EFFECTS.CROP_ID\n" +
+            "LEFT JOIN CROP_SOIL_TYPES\n" +
+            "ON FIELDS.SOILTYPE = CROP_SOIL_TYPES.SOIL_TYPES AND FIELDS.CROP_ID = CROP_SOIL_TYPES.CROP_ID\n" +
+            "WHERE FIELDS.TIMELEFT = 0 AND FIELDS.CROP_ID IS NOT NULL),\n" +
+            "\n" +
+            "NO_DUPLICATE AS (SELECT\n" +
+            "    ID, farm, price, size, soil_types, MAX(effects) AS max, MIN(CASE WHEN effects >= 2 THEN effects " +
+            "ELSE NULL END) AS min\n" +
+            "FROM\n" +
+            "    NEW_TABLE\n" +
+            "GROUP BY\n" +
+            "    ID\n" +
+            "),\n" +
+            "\n" +
+            "DOUBLE_YIELDS AS (SELECT * FROM NO_DUPLICATE WHERE MAX = 3 AND SOIL_TYPES IS NOT NULL),\n" +
+            "\n" +
+            "HALF_YIELDS AS (SELECT * FROM NO_DUPLICATE WHERE (MAX = 2 AND SOIL_TYPES IS NULL) OR " +
+            "(MAX = 3 AND MIN = 2 AND SOIL_TYPES IS NULL)),\n" +
+            "\n" +
+            "STANDARD_YIELD AS (SELECT * FROM NO_DUPLICATE WHERE (SOIL_TYPES IS NULL AND MAX IS NULL)" +
+            " OR (MAX IN (0,1,2) OR MAX IS NULL) AND NOT (MAX = 2 AND SOIL_TYPES IS NULL) " +
+            "OR (MAX = MIN AND SOIL_TYPES IS NULL))";
 
     @Query(value = tablesToFindTotal
             + " SELECT SUM(PRICE*SIZE*2) FROM DOUBLE_YIELDS WHERE FARM = :FARM_ID", nativeQuery = true)
